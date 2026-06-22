c79308738f76d76c8dc22d81852770f33b34afbd
gdanzz535-byte <gdanzz535@gmail.com>
2026-06-22 18:26:42 +0800

    Add files via upload

--- Database/config.js
+++ Database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tokens: "8372313890:AAFosLjfG3d_svFjLOLvEZVkYoXclPhA3fE:",
-  owner: "8353486366",
-  port: "4401",
-  ipvps: "https://onyxcore-production.up.railway.app"
+  tokens: "8500100959:AAGNyd4wyvDFREo4TLR1mw_ylqhvixyH5d4",  // Masukin Bot token kamu
+  owners: "7979381685", // Masukin ID Telegram kamu
+  port: 2121, // Masukin Port panel kamu 
+  ipvps: "http://create.fanzhosting.my.id" // Masukin IP vps kamu atau domain panel kamu
 };
