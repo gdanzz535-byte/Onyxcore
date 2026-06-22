ae66cbed9de3553d916911c3b31fbfb2af1e8064
gdanzz535-byte <gdanzz535@gmail.com>
2026-06-22 11:15:41 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tokens: "8372313890:AAFosLjfG3d_svFjLOLvEZVkYoXclPhA3fE:",  // Ubah Jadi Token Bot Mu !!!
-  owner: "8353486366", // Ubah Jadi Id Mu !!!
-  port: "4401", // Ubah Jadi Port Panel Mu !!!
-  ipvps: "https://dikaxmybini.privat.walzcfxcloud.web.id" // Ubah Jadi Ip Vps Mu !!!
-};
